ed19167d780b09b09a90f3db2c37da006077d068
凤之兮原 <1577310596@qq.com>
2023-08-24 16:12:33 +0800

    chore: add unplugin vue component and init tsconfig

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-import A from './components/A'
+import A from '@/components/A'
+const a = ref(0)
 </script>
 
 <template>
   <div text="red">123</div>
   <A />
+  {{ a }}
 </template>
 
 <style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,27 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Unocss from 'unocss/vite'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		vue(),
-		Unocss({
-			configFile: './unocss.config.ts',
-		}),
-		vueJsx(),
-	],
+  plugins: [
+    vue(),
+    Unocss({
+      configFile: './unocss.config.ts'
+    }),
+    vueJsx(),
+    AutoImport({
+      imports: ['vue']
+    }),
+    Components({
+      dts: true
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
 })
