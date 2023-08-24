9d9b6349949c1afcae7beaec6acf994ed749fe30
凤之兮原 <1577310596@qq.com>
2023-08-24 16:34:11 +0800

    chore: add basic Layout

--- src/components/A.tsx
+++ src/components/A.tsx
@@ -1,11 +1,5 @@
+import Base from '@/Layout/Base'
+
 export default () => {
-  return (
-    <a-space wrap>
-      <a-button type="primary">Primary Button</a-button>
-      <a-button>Default Button</a-button>
-      <a-button type="dashed">Dashed Button</a-button>
-      <a-button type="text">Text Button</a-button>
-      <a-button type="link">Link Button</a-button>
-    </a-space>
-  )
+  return <Base></Base>
 }
