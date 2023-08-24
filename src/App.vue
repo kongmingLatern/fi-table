96f07ef9d2b827c400f2074b488fab5493229c4c
凤之兮原 <1577310596@qq.com>
2023-08-24 15:53:36 +0800

    chore: clear and setup

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import A from './components/A'
+</script>
 
-<template>123</template>
+<template>
+	<div text="red">123</div>
+	<A />
+</template>
 
 <style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Unocss from 'unocss/vite'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+	plugins: [
+		vue(),
+		Unocss({
+			configFile: './unocss.config.ts',
+		}),
+		vueJsx(),
+	],
 })
