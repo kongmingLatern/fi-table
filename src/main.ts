df1cfb943c8f87b4fa581d124f3cfaf10d37d26e
凤之兮原 <1577310596@qq.com>
2023-08-31 11:00:34 +0800

    chore: init pinia store

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import Antd from 'ant-design-vue'
 import router from './router'
 import 'ant-design-vue/dist/reset.css'
 import 'uno.css'
 
-createApp(App).use(Antd).use(router).mount('#app')
+createApp(App)
+  .use(Antd)
+  .use(router)
+  .use(createPinia())
+  .mount('#app')
