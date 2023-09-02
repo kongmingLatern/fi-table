3941ec2e7166b09117340fa59176c4694fd42a1d
凤之兮原 <1577310596@qq.com>
2023-09-02 10:03:38 +0800

    feat: add Select component

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,43 +1,56 @@
+import { omit } from '@/utils'
 import { SelectProps } from 'ant-design-vue'
-import { defineComponent } from 'vue'
+import { PropType, defineComponent } from 'vue'
+interface SelectType {
+  value?: Ref<String>
+  options?: SelectProps['options']
+  onFocus?: () => void
+  onChange?: () => void
+}
 export default defineComponent({
   name: 'Select',
-  setup() {
-    const value = ref('')
-    const options = ref<SelectProps['options']>([
-      {
-        value: 'jack',
-        label: 'Jack'
-      },
-      {
-        value: 'lucy',
-        label: 'Lucy'
-      },
-      {
-        value: 'disabled',
-        label: 'Disabled',
-        disabled: true
-      },
-      {
-        value: 'yiminghe',
-        label: 'Yiminghe'
-      }
-    ])
+  props: {
+    value: {
+      type: Object as PropType<Ref<string>>,
+      default: undefined
+    },
+    options: {
+      type: Array as PropType<SelectProps['options']>,
+      default: []
+    },
+    onFocus: {
+      type: Function,
+      default: undefined
+    },
+    onChange: {
+      type: Function,
+      default: undefined
+    },
+    cfg: {
+      type: Object as PropType<SelectType>,
+      default: undefined
+    }
+  },
+  setup(props) {
+    const { cfg } = props
+    const value = cfg?.value || props.value || ref('')
+
     const focus = () => {
       console.log('focus')
     }
 
-    const handleChange = (value: string) => {
-      console.log(`selected ${value}`)
+    const handleChange = (val: string) => {
+      value.value = val
+      console.log(`selected ${val}`)
     }
     return () => (
       <a-select
         ref="select"
         v-model:value={value.value}
-        style="width: 120px"
-        onFocus={focus}
-        onChange={handleChange}
-        options={options.value}
+        onFocus={props.onFocus || focus}
+        onChange={props.onChange || handleChange}
+        options={props.options}
+        {...omit(cfg!, ['value'])}
       />
     )
   }
